96fb6b3793447cefdd87989e29992f9fa226cde1
Rajesh97266 <rajesh97266@gmail.com>
2024-12-29 20:10:42 +0530

    route-structure

--- main.js
+++ main.js
@@ -8,6 +8,32 @@ app.get("/", (req, res) => {
         message: "Hello world"});
 });
 
+//CRUD functionality of movies 
+
+//Reading
+app.get("/movies", (req, res) => {
+    res.json({
+        message: "Hello world"});
+});
+
+//Creating
+app.post("/movies", (req, res) => {
+    res.json({
+        message: "Hello world"});
+});
+
+//Updating
+app.put("/movies/:id", (req, res) => {
+    res.json({
+        message: "Hello world"});
+});
+
+//Deleting  
+app.delete("/movies/:id", (req, res) => {
+    res.json({
+        message: "Hello world"});
+});
+
 app.listen(port, () => {
     console.log(`The server is running at http://localhost:${port}`);
 });
